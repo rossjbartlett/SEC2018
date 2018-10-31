a1a0e7a560c94eb1c326c68475324b0cee6dadf3
unknown <antoineb374@gmail.com>
2018-10-31 12:16:53 -0600

    made test file

---
+++ src/sec2018/test.java
@@ -0,0 +1,13 @@
+package sec2018;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
