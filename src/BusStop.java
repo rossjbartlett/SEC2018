51572a0cb7854323e0df9a7907f9826ec5bc19f7
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-03 08:50:49 -0600

    Created class BusStop - Dylan

---
+++ src/BusStop.java
@@ -0,0 +1,22 @@
+
+
+public class BusStop {
+	
+	private int i, j;
+	
+	BusStop(int i, int j)
+	{
+		this.i = i;
+		this.j = j;
+	}
+	
+	public int getI()
+	{
+		return i;
+	}
+	
+	public int getJ()
+	{
+		return j;
+	}
+}
