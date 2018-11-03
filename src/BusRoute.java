5d6613f0451800c2d0ec472ede7df496c981b516
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-03 09:58:44 -0600

    Changed class BusRoute - Dylan

--- src/BusRoute.java
+++ src/BusRoute.java
@@ -3,17 +3,23 @@ import java.util.ArrayList;
 public class BusRoute {
 	
 	private ArrayList<BusStop> stops;
+	private ArrayList<BusStop> route;
 	private ArrayList<Bus> busses;
 	
 
-	public BusRoute(ArrayList<BusStop> stops) {
-		this.stops = stops;
+	public BusRoute() {
+		algorithm = new RouteAlgorithm();
 	}
 	
 	
 	//getters and setters
 	public ArrayList<Bus> getBusses() {
 		return busses;
+	}
+	
+	public void AddToRoute(InputLine line)
+	{
+		algorithm.AddToRoute(line);
 	}
 
 	public void setBusses(ArrayList<Bus> busses) {
